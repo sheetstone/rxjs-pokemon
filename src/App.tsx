9bf0e8494e85dc152ccb875650784bdca6b39953
Hong Zhang <sheetstone@gmail.com>
2022-01-24 16:00:43 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useMemo } from "react";
+import { useObservableState } from "observable-hooks";
+
+import "./App.css";
+
+import { pokemon$, selected$, deck$, Pokemon } from "./store";
+
+const Deck = () => {
+  const deck = useObservableState(deck$, []);
+  return (
+    <>
+      <h4>Deck</h4>
+      <div></div>
+    </>
+  );
+};
+
+const Search = () => {
+  const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const sub = pokemon$.subscribe(setPokemon);
+    return () => sub.unsubscribe();
+  }, []);
+
+  const filteredPokemon = useMemo(() => {
+    return pokemon.filter((p) =>
+      p.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [search, pokemon]);
+
+  return (
+    <div className="grid">
+      <input
+        type="text"
+        value={search}
+        className="header"
+        onChange={(evt) => {
+          setSearch(evt.target.value);
+        }}
+      />
+      <div>
+        {filteredPokemon.map((p) => (
+          <div key={p.name}>
+            <input
+              type="checkbox"
+              checked={p.selected}
+              onChange={() => {
+                if (selected$.value.includes(p.id)) {
+                  selected$.next(selected$.value.filter((id) => id !== p.id));
+                } else {
+                  selected$.next([...selected$.value, p.id]);
+                }
+              }}
+            />
+            <strong>{p.name}</strong> - {p.power}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App grid">
+      <div>{Search()}</div>
+      <div>{Deck()}</div>
     </div>
   );
 }
